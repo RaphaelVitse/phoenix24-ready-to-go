c3f99a323391acafc41a56670c7b2afea7f8dfc6
Raphael Vitse <raphaelv@MacBook-Pro-de-raphael.local>
2024-11-13 18:35:45 +0100

    change icone rocket  pour une image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import rocket from "../assets/fusee.png";
+
 const Header = () => {
   return (
     <>
       <header>
-        <i className="fa-solid fa-rocket"></i>
+        <img src={rocket} alt="" />
         <h1>Ready To Go</h1>
       </header>
     </>
